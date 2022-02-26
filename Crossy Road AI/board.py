75fec61f878ffb01f71a5b3c6c5d0fdbef6337f6
Anand Parekh <87543904+Onion38@users.noreply.github.com>
2022-02-26 14:32:54 -0600

    Add files via upload

---
+++ Crossy Road AI/board.py
@@ -0,0 +1,13 @@
+class Board:
+
+
+    def __init__(self):
+        self.board = self.generate_board()
+
+    def generate_board(self):
+        pass
+
+    
+
+    def move(self):
+        pass
---
+++ Crossy Road AI/stage.py
@@ -0,0 +1,8 @@
+from enum import Enum, auto
+
+class Stage(Enum):
+    HOME = auto()
+    GAME = auto()
+    LEADERBOARD = auto()
+    ENDGAME = auto()
+
